aa46f5d9a91e37c16eeb887aab967a39e5010490
Sergio <sergiocelemin1998@gmail.com>
2020-07-30 17:04:14 -0500

    add devise and simple token auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # Renders and HTML page with debugging information
   config.debug_exception_response_format = :default
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
